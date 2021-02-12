c5e3f0b56b29e37bc34f6d8db9633fbdb810e9a7
koo5 <kolman.jindrich@gmail.com>
2021-02-12 20:56:24 +0100

    some styles..

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,35 @@
-<h1>svelte-spa-router sample</h1>
-<h2>Basic routing</h2>
-
 <ul>
     <li><a href="#/">Home</a></li>
     <li><a href="#/test1">Test1</a></li>
     <li><a href="#/hello/svelte">Say hi!</a></li>
     <li><a href="#/wild/card">Wildcard route</a></li>
-    <li><a href="#/does/not/exist">Not found</a></li>
 </ul>
-
+<br><hr>
 <Router {routes} />
 
 <script>
     import Router from 'svelte-spa-router';
     import routes from './routes'
 </script>
+<style>
+	ul {
+		margin: 0;
+		padding: 0;
+        list-style-type: none;
+	}
+
+	li {
+		float: left;
+	}
+
+li a {
+  text-align: center;
+  padding: 1em;
+}
+
+li a:hover {
+    text-decoration: underline overline;
+}
+
+</style>
+
