44d56cd3f5e57b06671daa33c2585777497b3f19
koo5 <kolman.jindrich@gmail.com>
2021-02-12 21:06:57 +0100

    ..

--- src/routes/Test1.svelte
+++ src/routes/Test1.svelte
@@ -7,7 +7,10 @@ $: my_store_subscribe_method2 = my_store.subscribe;
 <button on:click='{() => {my_store += 2;my_store_subscribe_method = my_store.subscribe}}'>Add two</button><br>
 
 $my_store = {$my_store}<br>
-<i>- that's expected.</i><br><br>
+<i>- as expected, at first.</i><br><br>
+
+typeof my_store = {typeof my_store}<br>
+<i>- first object, then string.</i><br><br>
 
 my_store = {my_store}<br>
 <i>- as expected at first, but what's the 22222's?.</i><br><br>
@@ -21,5 +24,5 @@ my_store_subscribe_method2 = {my_store_subscribe_method2}<br><br>
 
 
 
-<a href="https://github.com/koo5/understand_humans/blob/master/delogic/test1/src/test1.svelte">source</a>
+<a href="https://github.com/koo5/svelte-capacitor/blob/testing2/src/routes/Test1.svelte">source</a>
 
